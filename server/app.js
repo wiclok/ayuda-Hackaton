cc2a6ff8b5ef5605a9b297472a435017831a7389
Cespedes Brian <cespedes.aguilera.brian@gmail.com>
2024-08-09 15:07:04 -0300

    basic server configuration

---
+++ server/app.js
@@ -0,0 +1,45 @@
+// Importaciones
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import fileUpload from 'express-fileupload';
+import { createServer } from 'http';
+import { Server as SocketServer } from 'socket.io';
+
+// Configuración
+const app = express();
+
+const httpServer = createServer(app);
+const io = new SocketServer(httpServer);
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'))
+app.use(helmet())
+app.use(fileUpload)
+
+// Rutas
+
+// Socket.io
+
+io.on('connection', (socket) => {
+  console.log('Cliente conectado', socket.id);
+
+  socket.emit('message', 'Bienvenido al chat');
+
+  socket.on('new-message', (data) => {
+    console.log(data);
+    io.emit('new-message', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Cliente desconectado', socket.id);
+  });
+});
+
+httpServer.listen(3000, () => {
+  console.log(`Server on http://localhost:${3000}`);
+  // startDb()
+});
