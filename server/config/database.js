33c7d9fc051c5250a56f8a00ea63bba999f54508
Cespedes Brian <cespedes.aguilera.brian@gmail.com>
2024-08-09 15:17:59 -0300

    basic configuration for a mysql database connection

---
+++ server/config/database.js
@@ -0,0 +1,13 @@
+import { Sequelize } from 'sequelize';
+import { environments } from './environments.js';
+
+export const sequelize = new Sequelize(
+  environments.DB.NAME,
+  environments.DB.USER,
+  environments.DB.PASSWORD,
+  {
+    host: environments.DB.HOST, 
+    dialect: environments.DB.DIALECT,
+    port: environments.DB.PORT,
+  }
+)
