ff837d448bbbbc814dd1cd25f5a440d27c7773b9
brian Céspedes aguilera <cespedes.aguilera.brian@gmail.com>
2024-08-11 20:56:04 -0300

    Added validations for registration and login endpoints

---
+++ server/models/schemas/auth.schemas.js
@@ -0,0 +1,65 @@
+import { body } from 'express-validator';
+
+export const registerUserSchema = [
+  body('name')
+    .exists()
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage('El nombre no debe estar vacio')
+    .isString()
+    .withMessage('El nombre solo puede estar en formato texto'),
+  body('last_name')
+    .exists()
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage('El apellido no debe estar vacio')
+    .isString()
+    .withMessage('El apellido solo puede estar en formato texto'),
+  body('username')
+    .exists()
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage('el nombre de usuario no debe estar vacio')
+    .isAlphanumeric()
+    .withMessage('el nombre de usuario debe ser alphanumerico')
+    .isLength({min: 5, max: 20})
+    .withMessage(
+      'El nombre de usuario debe tener un minimo de 5 caracteres y un maximo de 20.'
+    ),
+  body('email')
+    .exists()
+    .trim()
+    .notEmpty()
+    .withMessage('El email no debe estar vacio')
+    .isEmail()
+    .withMessage('El email debe tener formato de correo valido.'),
+  body('password')
+    .exists()
+    .trim()
+    .notEmpty()
+    .withMessage('La contraseña no debe estar vacia')
+    .isString()
+    .isLength({min: 8, max: 20})
+    .withMessage('Debe tener un mínimo de 8 caracteres y un máximo de 20.'),
+]
+
+export const loginUserSchema = [
+  body('email')
+    .exists()
+    .trim()
+    .notEmpty()
+    .withMessage('El email no debe estar vacio.')
+    .isEmail()
+    .withMessage('El email debe tener formato de correo valido.'),
+  body('password')
+    .exists()
+    .trim()
+    .notEmpty()
+    .withMessage('La contraseña no debe estar vacia.')
+    .isString()
+    .isLength({min: 8, max: 20})
+    .withMessage('Debe tener un mínimo de 8 caracteres y un máximo de 20.'),
+];
--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { loginUser, registerUser } from '../controllers/auth.constroller.js'
+import { loginUserSchema, registerUserSchema } from '../models/schemas/auth.schemas.js';
+import { validator } from '../middleware/validator.js'
 
 const authRouter = Router()
 
-authRouter.post('/register', registerUser)
-authRouter.post('/login', loginUser)
+authRouter.post('/register', registerUserSchema, validator, registerUser)
+authRouter.post('/login', loginUserSchema, validator, loginUser)
 
 export default authRouter
